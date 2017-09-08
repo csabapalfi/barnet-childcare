9e8f8d06a04b51a5148a14682b34fa7648998295
Csaba Palfi <csaba@palfi.me>
2017-09-08 21:27:59 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,17 @@
+# Barnet childcare by location
+
+Finding a nursery in London is tricky.
+
+This is a script to scrape [London Borough of Barnet - Childcare by location search](https://familyservices.barnet.gov.uk/PublicEnquiry/Search.aspx?searchID=4) as it's really slow and returns 5 results per page.
+
+## usage
+
+* install node.js >= 8
+* clone repo
+* `npm install`
+* `node index.js "<postcode>" <miles radius>`
+* paste `results.html` into a Google Spreadsheet for your own filtering
+
+## how
+
+Uses headless chromium via [puppeteer](https://github.com/GoogleChrome/puppeteer) from the Google Chrome team.
---
+++ index.js
@@ -0,0 +1,44 @@
+const { appendFile, writeFile } = require('fs');
+const { promisify } = require('util');
+const puppeteer = require('puppeteer');
+
+const search = async (page, postcode, distance) => {
+    const formPrefix = '#ctl00_ContentPlaceHolder1_Wizard1';
+    await page.click(`${formPrefix}_ctl01__rpc1tb`);
+    await page.type(postcode);
+    await page.click(`${formPrefix}_ctl01__rr1tb`);
+    await page.type(distance);
+    await page.click(`${formPrefix}_StartNavigationTemplateContainerID_StartNextLinkButton`);
+}
+
+const scrape = async (page) => {
+   return page.evaluate(() => document.querySelector('tbody').innerHTML);
+}
+
+async function run(postcode, radius) {
+  const browser = await puppeteer.launch({
+    // headless: false,
+  });
+  const page = await browser.newPage();
+  await page.setViewport({ width: 1920, height: 1080 });
+
+  await promisify(writeFile)('results.html', '<table>', 'utf-8');
+
+  await page.goto('https://familyservices.barnet.gov.uk/PublicEnquiry/Search.aspx?searchID=4');
+  await search(page, postcode, radius);
+  console.log('scraping page 1');
+  await page.waitForNavigation();
+  await promisify(appendFile)('results.html', await scrape(page), 'utf-8');
+
+  for (let i = 2; i <= 115; i++) {
+    console.log(`scraping page ${i}`);
+    await page.evaluate((i) => __doPostBack(`ctl00$ContentPlaceHolder1$topPager$${i}`,''), i);
+    await page.waitForNavigation();
+    await promisify(appendFile)(`results.html`, await scrape(page), 'utf-8');
+  }
+
+  browser.close();
+}
+
+const [,,...args] = process.argv;
+run(...args);
